75e7c2fed402e5ec7846fda5b3515928793aef67
coding-JUDAS <coding.judas@gmail.com>
2024-09-27 11:24:47 +0200

    new additions on variables and keywords

--- conditionalExecution.js
+++ conditionalExecution.js
@@ -26,3 +26,15 @@ console.log();
 
 // Switch Statement
 console.log('Switch Statement');
+var gender = "F";
+
+switch(gender){
+    case "M":
+        console.log("Male");
+        break;
+    case "F":
+        console.log("Female");
+        break;
+    default:
+        console.log(Unkown);
+}
--- variables.js
+++ variables.js
@@ -31,3 +31,41 @@ console.log(typeof balance);
 console.log(typeof dob);
 console.log(typeof person);
 console.log(typeof empty);
+console.log();
+
+// var vs let vs const keywords
+// var
+console.log("using 'var' to declare variables")
+for(var i = 0; i <= 5; i++){
+    console.log(i);
+}
+console.log("value of i outside the loop " + i);
+console.log();
+
+// let
+console.log("using 'let' to declare variables")
+for(let j = 0; j <= 5; j++){
+    console.log(j);
+}
+//console.log("value of j outside the loop " + j);
+
+// const
+//let brand = "CodingJudas";
+//brand  = {};
+//brand = 10;
+//brand = function(){
+    //return "Hello";
+//}
+const brand = "CodingJudas" // unable to reassign
+const brandObject = {};
+console.log(brandObject);
+brandObject["name"] = brand;
+console.log(brandObject);
+delete brandObject.name;
+console.log(brand);
+console.log(brandObject);
+
+/*
+Always start by using the const keyword for naming variables and functions.
+use the let keyword if you need to reassign, e.g for loops.
+ */
